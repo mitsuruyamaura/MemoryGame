1df900dd9c3bedd7a699480af4597d2bf396c52c
mitsuru_yamaura <komayou326@gmail.com>
2025-09-22 15:27:55 +0900

    その２

---
+++ Assets/Editor/BackPackInItemCollectionPreview.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEditor;
+using R3;
+using ObservableCollections;
+
+// EditorGUILayout.ObjectField
+// https://docs.unity3d.com/ja/current/ScriptReference/EditorGUILayout.ObjectField.html
+
+
+/// <summary>
+/// ReactiveCollection の要素をインスペクターに表示するためのエディター拡張
+/// </summary>
+[CustomEditor(typeof(PlayerInventoryManager), true)]
+public class BackPackInItemCollectionPreview : Editor {
+    private BackPackInItem addItem;
+
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        // 親クラスを参照
+        PlayerInventoryManager _drc = (PlayerInventoryManager)target;
+        ObservableList<BackPackInItem> _iCol = _drc.PlayerBackPackItemList;
+
+        int _count = _iCol.Count;
+        if (_count == 0) {
+            EditorGUILayout.HelpBox("No items or No runtime.", MessageType.None);
+            return;
+        }
+
+        // リストの表示
+        for (int i = 0; i < _count; i++) {
+            EditorGUILayout.BeginHorizontal();
+
+            // BackPackInItem の各フィールドを表示(allowSceneObjects の指定をしないと非推奨の警告が出る)
+            _iCol[i] = (BackPackInItem)EditorGUILayout.ObjectField(obj: _iCol[i], typeof(BackPackInItem), allowSceneObjects : false);   // as BackPackInItem;
+
+            if (GUILayout.Button("Delete")) {
+                _iCol.RemoveAt(i);
+                EditorGUILayout.EndHorizontal();
+                return;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // 追加のGUI
+        EditorGUILayout.LabelField(" ");
+        EditorGUILayout.BeginHorizontal();
+        addItem = (BackPackInItem)EditorGUILayout.ObjectField("Add Item ->", addItem, typeof(BackPackInItem), allowSceneObjects : false);
+        if (GUILayout.Button("Add")) {
+            if (addItem != null) {
+                _iCol.Add(addItem);
+                addItem = null;
+            }
+            EditorGUILayout.EndHorizontal();
+            return;
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
+
+public class DebugReactiveCollection : MonoBehaviour {
+    public ObservableList<BackPackInItem> backPackInItemCollection = new ();
+
+    private void Start() {
+        // テスト用データの追加
+        backPackInItemCollection.Add(new BackPackInItem());
+        backPackInItemCollection.Add(new BackPackInItem());
+
+        // UniRxの処理
+        backPackInItemCollection.ObserveRemove()
+            .Subscribe(i => Debug.Log("Remove Item:" + i.Value.itemData));
+
+        backPackInItemCollection.ObserveAdd()
+            .Subscribe(i => Debug.Log("Add Item :" + i.Value.itemData));
+    }
+}
---
+++ Assets/Editor/ResetManagerEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+[CustomEditor(typeof(ResetManager))]
+public class ResetManagerEditor : Editor {
+
+    //SerializedProperty useResetEveryDay;
+    //SerializedProperty resetHour;
+    //SerializedProperty resetDateString;
+    //SerializedProperty resetDay;
+
+    //private void OnEnable() {
+    //    // SerializedProperty によるプロパティ取得
+    //    useResetEveryDay = serializedObject.FindProperty("useResetEveryDay");
+    //    resetHour = serializedObject.FindProperty("resetHour");
+    //    resetDateString = serializedObject.FindProperty("resetDateString");
+    //    resetDay = serializedObject.FindProperty("resetDay");
+    //}
+
+    //public override void OnInspectorGUI() {
+    //    // 対象オブジェクトの取得
+    //    ResetManager resetManager = (ResetManager)target;
+
+    //    // SerializedObject の更新
+    //    serializedObject.Update();
+
+    //    // 毎日リセットのトグル設定
+    //    EditorGUILayout.PropertyField(useResetEveryDay, new GUIContent("Use Reset EveryDay"));
+
+    //    // 毎日リセットが無効の場合、曜日選択表示
+    //    if (!useResetEveryDay.boolValue) {
+    //        //resetManager.resetDay = (DayOfWeek)EditorGUILayout.EnumPopup("Reset Day", resetManager.resetDay);
+    //        EditorGUILayout.PropertyField(resetDay, new GUIContent("Reset Day"));
+    //    }
+
+    //    // 時間設定 (0〜23)
+    //    EditorGUILayout.IntSlider(resetHour, 0, 23);
+
+    //    // リセット日時編集可能フィールド
+    //    EditorGUI.BeginChangeCheck();
+    //    string newDateString = EditorGUILayout.TextField("Reset Date (Edit)", resetDateString.stringValue);
+
+    //    // 入力値のフォーマットチェックと反映
+    //    if (EditorGUI.EndChangeCheck()) {
+    //        DateTime parsedDate;
+    //        if (DateTime.TryParseExact(newDateString, "yyyy/MM/dd HH", null, System.Globalization.DateTimeStyles.None, out parsedDate)) {
+    //            resetDateString.stringValue = newDateString; // 入力値を保存
+    //            resetManager.SetDebugDate(parsedDate); // 仮日時も更新
+    //        } else {
+    //            EditorGUILayout.HelpBox("フォーマットが不正です！例: 2025/01/10 04", MessageType.Error);
+    //        }
+    //    }
+
+    //    // 変更を反映
+    //    serializedObject.ApplyModifiedProperties();
+    //}
+}
